7d0cbd4c28ae0baaf489d3a594d0548f0adb2bcd
HGKXZ <wrchxrt4712@gmail.com>
2025-03-23 01:40:14 +0700

    style: update NavBar styling and improve link accessibility; enhance session-based sign-in/out display

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,37 +6,39 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 export default async function NavBar() {
 
     const session = await getServerSession(authOptions)
+    console.log(session)
 
     return (
-        <nav className="bg-blue-900 p-4">
-        <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+        <nav className="bg-black p-4">
+        <div className="max-w-screen flex justify-between items-center">
             {/* Logo */}
-            <a href="/">
+            <a href="/" className="mr-8">
                 <div className="text-white text-2xl font-bold">STGP</div> 
-                <div className="text-white text-md font-">HotelBooking</div>
+                <div className="text-white text-md">HotelBooking</div>
             </a>
 
-            {/* Desktop Menu */}
-            <ul className="hidden md:flex space-x-8">
+            <ul className="flex space-x-8 items-center">
             <li>
-                <a href="/hotel" className="text-white hover:text-blue-300">
-                Hotel
+                <a href="#" className="font-sans font-semibold text-white hover:text-blue-300">
+                About
                 </a>
             </li>
             <li>
-                <a href="#" className="text-white hover:text-blue-300">
-                Booking
+                <a href="#" className="font-sans font-semibold text-white hover:text-blue-300">
+                Contact
                 </a>
             </li>
             <li>
-                <a href="#" className="text-white hover:text-blue-300">
-                About Us
+                <a href="/hotel" className="font-sans font-semibold text-white hover:text-blue-300">
+                Explore Stay
                 </a>
             </li>
             <li>
                 {
-                    session? <a href="/api/auth/signout" className="text-white hover:text-blue-300">Sign-Out{session.user?.name}</a>
-                    : <a href="/api/auth/signin" className="text-white hover:text-blue-300">Sign-In</a>
+                    session? <a href="/api/auth/signout" className="border p-2 rounded-lg text-white font-sans font-semibold hover:bg-white hover:text-black whitespace-nowrap">
+                        Sign-Out {session.user?.name}</a>
+                    : <a href="/api/auth/signin" className="border p-2 rounded-lg text-white font-sans font-semibold hover:bg-white hover:text-black whitespace-nowrap">
+                        Sign-In / Register</a>
                 }
             </li>
             </ul>
