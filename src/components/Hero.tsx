6988062b6b7c07569d364f8b3d7db00d3b25ad3b
wirepecion <sira.siravut@gmail.com>
2025-03-24 23:39:59 +0700

    feat: refactor Hero component to use Button component and improve layout responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,23 +1,34 @@
+import Button from "./Button";
 export default function Hero() {
     return (
-      <section className="grid grid-cols-12 max-w-[75%] mx-auto gap-x-[15px] pt-36 pb-36 bg-[#000235] ">
+        <section className="grid grid-cols-12 gap-[15px] w-[1065px] mx-auto pt-32 pb-36 bg-[#000235]">
         {/* Left content */}
         <div className="col-span-12 md:col-span-6 space-y-6">
           <h1 className="text-8xl text-white md:text-7xl font-Outfit font-bold leading-tight">
             Find And <br /> Book Your <br /> Perfect Stay
           </h1>
-          <p className="text-base text-white font-roboto font-medium pt-7">
+          <p className="text-lg text-white font-roboto font-weight-600 pt-7">
             The most seamless and secure way to book unique hotel experiences around the world.
           </p>
-          <div className="col-span-12 md:col-span-4 flex space-x-4">
-            <button className="bg-orange-400 text-white rounded-md w-[200px]">Book Now</button>
-            <button className="border text-white border-white py-2 px-6 rounded-md">View Stays</button>
+                  {/* Button row with 5/12 width */}
+        <div className="grid grid-cols-12">
+        <div className="col-span-12 md:col-span-9 flex space-x-8 pt-3">
+            <Button variant="primary">BOOK NOW</Button>
+            <Button variant="black-outline">VIEW STAYS</Button>
           </div>
         </div>
+          
+          {/* button */}
+          {/* <div className="col-span-12 md:col-span-4 flex space-x-4">
+            <button className="bg-orange-400 text-white rounded-md y-2 px-6">Book Now</button>
+            <button className="border text-white border-white py-2 px-6 rounded-md">View Stays</button>
+            </div> */}
+        </div>
+
   
         {/* Right image */}
         <div className="col-span-12 md:col-span-6 flex justify-center">
-          <img src="/assets/hero-graphic.png" alt="Hero Graphic" className="w-80" />
+          <img src="/assets/hero-graphic.png" alt="Hero Graphic" className="w-auto h-auto" />
         </div>
       </section>
     );
