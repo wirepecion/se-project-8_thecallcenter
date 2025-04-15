d3c725b47d9d04f75c847724e84974f0e63cd79d
wirepecion <sira.siravut@gmail.com>
2025-04-15 21:34:10 +0700

    feat: add Map image to HotelPage and improve RoomCard and RoomCatalog layouts

--- src/components/HotelPage.tsx
+++ src/components/HotelPage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import HotelCard from "./HotelCard";
 
 export default async function HotelPage({ hotelDetail }: { hotelDetail: Promise<HotelResponse> }) {
-    
+
     const hotel = (await hotelDetail).data;
 
     return (
@@ -13,22 +13,33 @@ export default async function HotelPage({ hotelDetail }: { hotelDetail: Promise<
             <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
                 <div className="relative w-full md:w-1/3 h-[20vh] md:h-auto rounded-lg overflow-hidden shadow-lg">
                     <HotelCard
-                    hotelName={""}
-                    address={""}
-                    imgSrc={hotel.picture || "/img/hotel.jpg"}
+                        hotelName={""}
+                        address={""}
+                        imgSrc={hotel.picture || "/img/hotel.jpg"}
                     />
                 </div>
-                
+
                 <div className="flex-1 bg-white p-6 rounded-lg shadow-lg">
-                    <div className="text-lg text-black space-y-2">
-                        <p><span className="font-semibold">Name:</span> {hotel.name}</p>
-                        <p><span className="font-semibold">Address:</span> {hotel.address}</p>
-                        <p><span className="font-semibold">Tel:</span> {hotel.tel}</p>
+                    <div className="text-lg text-black space-y-2 text-left">
+                        <p className="font-outfit font-medium text-left text-8xl">{hotel.name}</p>
+                        <p className="font-medium text-slate-700">Address : {hotel.address}</p>
+                        <p className="font-medium text-slate-700">Tel : {hotel.tel}</p>
                     </div>
+                    <div className="relative w-full h-[21vh] rounded-lg overflow-hidden shadow-lg mt-4">
+                        <Image
+                            src="/img/Map.jpg"
+                            alt="Map image"
+                            fill
+                            className="object-cover rounded-lg"
+                            priority
+                        />
+                    </div>
+
                 </div>
+
             </div>
 
-            <div className="mt-8">
+            <div className="mt-12">
                 <RoomCatalog rooms={hotel.rooms} />
             </div>
         </div>
