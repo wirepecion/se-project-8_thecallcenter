2dcefddae52147177e19d8e1a68eada7b65f2203
kwank6704 <nattarat.kwan@gmail.com>
2025-04-23 15:13:57 +0700

    Add user profile components for credits, discounts, and ranks
    - Implemented CreditsCard component to display user's credit balance.
    - Created DiscountCard component to show membership discount based on points.
    - Developed RankCard component to display user's membership tier with corresponding image.
    - Added PNG images for platinum and silver ranks.
    - Integrated server session and user profile fetching for all components.

--- src/app/profile/[uid]/page.tsx
+++ src/app/profile/[uid]/page.tsx
@@ -2,28 +2,38 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../api/auth/[...nextauth]/authOptions";
 import getUserProfile from "@/libs/Auth/getUserProfile";
 import ProfileUserCard from '@/components/ProfileUserCard';
-import MemberShipCard from "@/components/MembershipCard";
 import MembershipCard from "@/components/MembershipCard";
+import CreditsCard from "@/components/CreditsCard";
+import DiscountCard from "@/components/DiscountCard";
+import RankCard from "@/components/RankCard";
 
 export default async function ProfilePage({ params }: { params: { uid: string } }) {
     const session = await getServerSession(authOptions);
     if (!session || !session.user.token) return null;
 
     const userProfile = await getUserProfile(session.user.token);
-
     const user = userProfile.data;
 
     return (
 
         
-        <div className="p-3 h-auto text-white min-h-screen w-[95%]">
+        <div className="min-h-screen w-full text-white py-10 px-4">
             
-            <div className="flex items-center justify-center min-h-screen">
-            <ProfileUserCard uid={params.uid} />
+            <div className="max-w-screen-lg mx-auto flex flex-col gap-12">
+                
+                <div className="p-2"/>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-6">
+                    <ProfileUserCard uid={params.uid} />
+                    <MembershipCard uid={params.uid} />
+                </div>
 
-            <MembershipCard uid={params.uid} />
-        </div>
-
+                <div className="flex flex-col md:flex-row items-center justify-center gap-6">
+                    <div className="p-[1px]"/>
+                    <RankCard uid={params.uid} />
+                    <DiscountCard uid={params.uid} />
+                    <CreditsCard uid={params.uid} />
+                </div>
+            </div>
         </div>
     );
 }
