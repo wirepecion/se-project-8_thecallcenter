47d33fab80025fcb27b9c4a2a686e2a7ef16186d
Warapong Thongkhundam <nat.n.dragon@gmail.com>
2025-04-26 00:35:33 +0700

    feat: enhance MembershipTable and RankCardmember components with improved tier visuals and layout

--- src/components/RankCardmember.tsx
+++ src/components/RankCardmember.tsx
@@ -12,26 +12,30 @@ export default function RankCardmember({
     
     const bgcolor = (() => {
         switch (rank) {
-            case 'Bronze': return 'bg-gradient-to-br from-[#A64F3C] via-[#D9805F] to-[#A64F3C]';
-            case 'Silver': return 'bg-gradient-to-br from-[#8594AE] via-[#DDDAE0] to-[#8594AE]';
-            case 'Gold': return 'bg-gradient-to-br from-[#D99E32] via-[#F2C744] to-[#D99E32]';
-            case 'Platinum': return 'bg-gradient-to-br from-[#C786EB] via-[#E2B3FC] to-[#C786EB]';
-            case 'Diamond': return 'bg-gradient-to-br from-[#3e8dc9] via-[#7dd7ff] to-[#3e8dc9]';
-            default: return 'bg-slate-200';
+            case 'Bronze':
+                return 'bg-gradient-to-br from-[#C17547] via-[#E0A58A] to-[#C17547]';
+              case 'Silver':
+                return 'bg-gradient-to-br from-[#AFBCCA] via-[#ECECEA] to-[#AFBCCA]';
+              case 'Gold':
+                return 'bg-gradient-to-br from-[#E5BA47] via-[#F7D96A] to-[#E5BA47]';
+              case 'Platinum':
+                return 'bg-gradient-to-br from-[#D09FED] via-[#F0CAF2] to-[#D09FED]';
+              case 'Diamond':
+                return 'bg-gradient-to-br from-[#65A8E4] via-[#B3E5FF] to-[#65A8E4]';
+              default:
+                return 'bg-slate-100';
 
         }
     });
 
     return (
         <div>
-            <div className={`${highlight ? bgcolor() : 'bg-white'} rounded-md shadow p-3  h-[80px]  flex justify-between items-center w-full max-w-md transition-transform duration-300 hover:scale-105 hover:shadow-lg`}>
+            <div className={`${highlight ? bgcolor() : 'bg-white'} rounded-md shadow p-3 w-[165px] h-[80px]  flex justify-between items-center  max-w-md `}>
                 <div className="flex flex-col items-start">
                     <p className="text-gray-800 font-outfit font-bold text-2xl">{rank}</p>
-                    {/* <p className="text-sm font-semibold text-blue-500 mt-1 ">
-                        → see more
-                    </p> */}
+                    
                 </div>
-                <div className=" w-14 h-14 relative">
+                <div className=" w-12 h-12 relative">
                     <Image
                         src={`/img/ranks/${rank.toLowerCase()}.png`}
                         alt={`${rank} rank`}
