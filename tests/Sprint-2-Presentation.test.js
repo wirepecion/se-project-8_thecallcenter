102526885391f148ed5305cadd3ea6e866e40a5f
PatcharaponOngkakul <6733170321@student.chula.ac.th>
2025-04-29 15:57:53 +0700

    feat: enhance Sprint 2 presentation test with admin login and user management scenarios

--- tests/Sprint-2-Presentation.test.js
+++ tests/Sprint-2-Presentation.test.js
@@ -47,7 +47,7 @@ test('Sprint 2 - EPIC 1 Run Through', async ({ page }) => {
   });
   await page.waitForTimeout(2000);
   await page.getByRole('link', { name: 'Nova Terrace Nova Terrace' }).click();
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(4000);
   await page.evaluate(() => {
     window.scrollBy(0, 600); // Scroll down by 600 pixels
   });
@@ -75,8 +75,8 @@ test('Sprint 2 - EPIC 1 Run Through', async ({ page }) => {
   /* US1-3 : Use own benefits to get discount for booking a room in hotel according to membership tier (should -5% discount(-750฿)) */
   await page.getByRole('radio', { name: 'Bank' }).check();
   await page.getByRole('combobox').selectOption('kbank');
-  await page.getByRole('textbox').first().click();
-  await page.getByRole('textbox').first().fill('123456789');
+  await page.getByRole('spinbutton').click();
+  await page.getByRole('spinbutton').first().fill('123456789');
 
   /* US1-3 : Use own credits to get extra discount for booking (should -1000฿ discount) */
   await page.waitForTimeout(2000);  
@@ -148,6 +148,84 @@ test('Sprint 2 - EPIC 1 Run Through', async ({ page }) => {
   await page.getByRole('button', { name: 'User avatar' }).click();
   await page.getByRole('link', { name: 'Sign out' }).click();
   await page.getByRole('button', { name: 'Log Out' }).click();
+  await page.pause();
 
-  
+  /* Log-in with admin account */
+  await page.getByRole('link', { name: 'Sign-In / Register' }).click();
+  await page.getByRole('textbox', { name: 'Enter your email' }).click();
+  await page.getByRole('textbox', { name: 'Enter your email' }).fill('admin@gmail.com');
+  await page.getByRole('textbox', { name: 'Enter your password' }).click();
+  await page.getByRole('textbox', { name: 'Enter your password' }).fill('123456');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  /* US 1-6 : View admin's profile and statistic */
+  await page.getByRole('link', { name: 'user Main Admin admin@gmail.' }).click();
+  await page.pause();
+
+  /* US 1-6 : View all users and their membership tiers */
+  await page.evaluate(() => {
+    window.scrollBy(0, 600); // Scroll down by 600 pixels
+  });
+  await page.locator('select').selectOption('bronze');
+  await page.waitForTimeout(2000);
+  await page.locator('select').selectOption('silver');
+  await page.waitForTimeout(2000);
+  await page.locator('select').selectOption('gold');
+  await page.waitForTimeout(2000);
+  await page.locator('select').selectOption('');
+  await page.waitForTimeout(2000);
+  await page.getByRole('combobox', { name: 'Select a User' }).click();
+  await page.getByRole('combobox', { name: 'Select a User' }).fill('ti');
+  await page.getByRole('option', { name: 'titan' }).click();
+  await page.waitForTimeout(2000);
+  await page.getByRole('cell', { name: 'user titan' }).click();
+  await page.getByRole('button', { name: 'User avatar' }).click();
+  await page.pause();
+
+  /* Update user's booking status to "completed" */
+  await page.getByRole('button', { name: 'User avatar' }).click();
+  await page.getByRole('link', { name: 'All Bookings' }).click();
+  await page.locator('.MuiButtonBase-root').first().click();
+  await page.waitForTimeout(2000);
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await page.waitForTimeout(2000);
+  await page.getByRole('combobox').selectOption('completed');
+  await page.waitForTimeout(2000);
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('button', { name: 'Yes, update!' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+
+  /* US 1-3 : View user's updated membership tier */
+  await page.locator('a').filter({ hasText: 'Main Adminadmin@gmail.com' }).click();
+  await page.waitForTimeout(2000);
+  await page.evaluate(() => {
+    window.scrollBy(0, 600); // Scroll down by 600 pixels
+  });
+  await page.waitForTimeout(2000);
+  await page.getByRole('combobox', { name: 'Select a User' }).click();
+  await page.getByRole('combobox', { name: 'Select a User' }).fill('ti');
+  await page.getByRole('option', { name: 'titan' }).click();
+  await page.getByRole('cell', { name: 'user titan' }).click();
+  await page.pause();
+
+  /* Log-out from admin account */
+  await page.getByRole('button', { name: 'User avatar' }).click();
+  await page.getByRole('link', { name: 'Sign out' }).click();
+  await page.locator('div').filter({ hasText: /^Log Out$/ }).click();
+  await page.pause();
+
+  /* Log-in with user's account */
+  await page.getByRole('link', { name: 'Sign-In / Register' }).click();
+  await page.getByRole('textbox', { name: 'Enter your email' }).click();
+  await page.getByRole('textbox', { name: 'Enter your email' }).fill('titan@gmail.com');
+  await page.getByRole('textbox', { name: 'Enter your password' }).click();
+  await page.getByRole('textbox', { name: 'Enter your password' }).fill('123456');
+  await page.getByRole('textbox', { name: 'Enter your password' }).press('Enter');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  /* US 1-3 : User view their own's updated membership tier */
+  await page.getByRole('link', { name: 'user titan titan@gmail.com' }).click();
+  await page.getByRole('link', { name: 'Membership' }).click();
+  await page.pause();
+
 });
